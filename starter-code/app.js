fd7e590351549f3e73cc67c0a81fec8860618aa8
philipwisner <philipwisner@gmail.com>
2017-09-10 20:49:33 +0200

    updated

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,46 +1,28 @@
 const express = require('express');
 const app = express();
-const Chuck  = require('chucknorris-io');
-const client = new Chuck();
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 
+const Chuck  = require('chucknorris-io');
+const client = new Chuck();
 
 app.use(cookieParser());
 app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/main-layout');
 app.set('view engine', 'ejs');
 
+
 app.get('/', (req,res,next) => {
-  res.render('index',{greeting: "HI!!!"});
-});
-
-app.get('/categories', (req,res,next) => {
-  client.getJokeCategories()
-  .then((response) =>  {
-    res.render('categories', {categoryType: response});
-  })
-  .catch((err)=> {
-    // handle error
-  });
-});
-
-app.get('/categories', (req,res,next) => {
-  client.getJokeCategories()
-  .then((response) =>  {
-    res.render('categories', {categoryType: response});
-  })
-  .catch((err)=> {
-    // handle error
-  });
+  res.render('index',{greeting: "Welcome"});
 });
 
 app.get('/random', (req,res,next) => {
-  console.log(req.method, req.path);
-
   client.getRandomJoke()
   .then((response) => {
     res.send('<p>'+ response.value + '</p>');
@@ -49,19 +31,37 @@ app.get('/random', (req,res,next) => {
   });
 });
 
-app.get('/hello', (req, res, next) => {
-  console.log("Ok");
-  res.send(`
-    <!doctype html>
-    <html>
-      <head>
-        <link rel="stylesheet" href="/stylesheet/style.css">
-      </head>
-      <body>
-        Yo Becht!
-      </body>
-    </html>
-  `);
+app.get('/categories', (req,res,next) => {
+  client.getJokeCategories()
+  .then((response) =>  {
+    res.render('categories', {categoryType: response});
+  })
+  .catch((err)=> {
+    res.send(error);
+  });
 });
+
+app.get('/categories/:category', (req,res,next) => {
+  client.getRandomJoke(req.params).then((response) =>  {
+    res.render('joke-by-category', repsonse);
+  })
+  .catch((err)=> {
+    res.send(error);
+  });
+});
+
+app.get('/search', (req, res, next) => {
+  res.render('search-form', { items : [{ value: null }]});
+ });
+
+app.post('/search', (req, res, next) => {
+  client.search(req.body.keyword)
+  .then(function (response) {
+    res.render('search-form', response);
+  }).catch(function (err) {
+    res.send(error);
+  });
+});
+
 
 app.listen(4000, () => console.log('Heard it Bech!!'));
